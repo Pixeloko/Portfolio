3767fc3318a0e16b0fc655b79e26518d5fa42895
Pixeloko <lola-louna@outlook.fr>
2026-02-08 12:44:45 +0100

    create new folder for geopolitics articles

---
+++ geopo/articles.js
@@ -0,0 +1,50 @@
+const articles = [
+  {
+    id: 1,
+    title:
+      "L’eau : source de richesse, pouvoir et de conflits",
+    description:
+      "Du contenu par les joueurs et pour les joueurs, ça fait penser aux expériences de Fortnite et ça pourrait bien débarquer sur GTA 6. Rockstar est d'ailleurs en discussion avec de nombreux créateurs du genre.",
+      subtitle1: "Rockstar inspiré par Fortnite et Roblox",
+    text1:
+      "Avec Grand Theft Auto VI, la nouvelle bombe de Rockstar Games annoncée pour cet automne, les fans pourraient être surpris des nouveautés concernant la création de contenu sur le mode online. Selon les propos de trois insiders rapportés par Digiday, le studio est actuellement en discussion avec de nombreux créateurs qui ont travaillé sur Fortnite, Roblox, ainsi que sur le mode en ligne de GTA V. Ce qui fait de l’œil à Rockstar, c'est le côté sandbox des deux premiers titres cités. On voit en effet qu’au fil des années, Fortnite s'est étendu : des modes de jeu naissent presque toutes les semaines grâce à la communauté. On oublierait même que le jeu est avant tout un battle royal. Il ne fait aucun doute que le modèle que proposera GTA 6 sera différent de ce que l’on connaît sur Fortnite, Roblox ou encore Minecraft. GTA 6 pourrait donc aller plus loin que ce que fait GTA 5 avec ses options RP et son système d’hébergement de serveurs tels que FiveM. Selon les dires de Chris Hopper, un expert de chez Loaded, une entreprise de conseil marketing : “les fondations de GTA RP se sont toujours basées sur l’interaction entre joueurs”.",
+    subtitle2: "Une belle opportunité pour les plus petits studios",
+    text2:
+      "L’article de Digiday explique que l’appel aux créateurs qu’entreprend Rockstar permettrait au studio de modifier l’environnement et, du côté des joueurs, d'apporter leur propre propriété intellectuelle et potentiellement leur marque. Cette intervention leur accorderait alors un rôle de sponsor du jeu. Des créations qui permettront de rendre plus attrayant le mode en ligne du futur GTA en offrant toujours plus de manières d’y jouer. Ce genre de collaboration pourrait bénéficier aussi bien à Rockstar, qui verrait le nombre de joueurs augmenter, qu'aux participants du programme qui gagneraient en renommée dans le domaine et en rentabilité. Par exemple, FiveM, que nous avons évoqué plus haut, propose ses services d’hébergement en échange d’une contribution sous forme d’abonnement, ce qui permet de rémunérer l’équipe de créateurs qui travaille dessus. D’autres se sont fait connaître grâce à ce type de collaboration. C’est le cas de JOGO, un studio de développement qui est à l’origine des modes Bed Wars et Super Red Vs Blue de Fortnite.",
+    source: "https://www.jeuxvideo.com/news/1971213/gta-6-rockstar-discute-avec-des-pros-de-fortnite-et-roblox-pour-creer-l-ultime-experience-sandbox.htm"
+  },
+  {
+    id: 2,
+    subject: "Nintendo",
+    length: "≈2100",
+    title:
+      "Nouveau leak sur la Nintendo Switch 2 : Ses Joy-Cons sont toujours plus surprenants...",
+    description:
+      "Les fuites d’informations concernant la prochaine console de Nintendo continuent, malgré une annonce officielle mi-janvier. La dernière en date concerne un brevet déposé par le constructeur repéré par les internautes. Un brevet qui cache une fonctionnalité... renversante.",
+    subtitle1: "Attachez les Joy-Cons dans le mauvais sens",
+    text1:
+      "Ce brevet partagé sur Reddit dévoile une nouvelle fonctionnalité des Joy-Cons. Ces derniers s’éloignent de plus en plus de leur modèle de base : sur l’image, les manettes sont dans le “mauvais sens”, ce qui suggère que l'on peut retourner l’écran pour pouvoir jouer. L’image se renversera en fonction de comment les Joy-Cons sont accrochés et non en fonction de comment la console sera tenue. Cette disposition a probablement été pensée pour un meilleur confort au moment de la prise en main en mode portable. Le fil des écouteurs ou du casque peut alors tomber en dessous. Sur la Switch première du nom et sur sa successeuse, il n’y a qu’un port audio. Cela dit, la nouvelle machine dispose de deux ports USB type C. On peut imaginer que la fonctionnalité pourrait servir pour des titres tels que Mario Party Superstars qui exploitent le potentiel des manettes dans les mini-jeux et jouent parfois avec l'orientation. Les anciens Joy-Cons ne seront pas utilisables sur la nouvelle machine.",
+    subtitle2: "La Nintendo touch : la révolution des Joy-Cons",
+    text2:
+      "Malgré la possibilité de retourner l’écran de la Switch 2, il semble y avoir un sens par défaut, surtout si l’on considère la présence d’un support arrière pour maintenir l’écran en position inclinée, sur table. On peut aussi noter le fait que les fentes pour insérer les manettes soient colorées aux couleurs du contour des Joy-Cons. Avec le trailer d’annonce de Nintendo, le public a découvert les grandes nouveautés concernant les périphériques de la Switch 2, comme le fait que ces derniers soient magnétiques et se clipsent simplement à l’écran. Au revoir donc aux rails usables où glisser les Joy-Cons. On sait également que, comme l'écran, les manettes sont plus grandes. Une dernière chose : une nouvelle fonctionnalité permettant de les faire glisser à l’image d’une souris. Cet autre brevet nous informe que les Joy-Cons sont dotés de capteurs optiques sur leur tranche. Les boutons R et ZR serviraient alors de clic droit, de clic gauche, et le joystick pour le défilement. Une révolution qui serait très intéressante pour les jeux de tir ou les MOBA (Multiplayer Online Battle Arena). Toujours est-il que les brevets, bien qu'ils soient authentiques, ne présagent pas de la présence de ces fonctionnalités sur le modèle final de la console. Il faudra attendre, au minimum, le premier grand aperçu de la Nintendo Switch 2 lors du Direct déjà planifié par son constructeur. Rendez-vous le 02 avril à 15h00 heure française.",
+    source:"https://www.jeuxvideo.com/news/1969516/nouveau-leak-sur-la-nintendo-switch-2-ses-joy-cons-sont-toujours-plus-surprenants.htm#:~:text=News%20jeu%20Nouveau%20leak%20sur,sont%20toujours%20plus%20surprenants...&text=Les%20fuites%20d'informations%20concernant,constructeur%20repéré%20par%20les%20Internautes."
+  },
+  {
+    id: 3,
+    subject: "Pokemon",
+    length: "≈2000",
+    title:
+      "Pokémon Pocket : l’une des surprises du Pokémon Presents a été gâchée, on connaît désormais le prochain booster et sa carte phare !",
+    description:
+      "Le Pokémon Presents est aujourd'hui à 15h ! Un jour avant sa diffusion, ce leaker a révélé le contenu du prochain set de cartes, et il va briller de mille feux. La fuite provient de TikTok et la vidéo a été vite supprimée, mais Internet n'oublie jamais.",    subtitle1: "Les leaks triompheront",
+    subtitle1: "Les leaks triompheront",
+    text1:
+      "Hier, les fans de Pokémon ont eu la surprise de découvrir en avant-première la prochaine collection de boosters de Pokémon Pocket. L’information provient de TikTok, où un post promotionnel semble avoir été malencontreusement publié beaucoup trop tôt par le compte officiel du jeu. On imagine que la vidéo aurait dû être diffusée en même temps que le Pokémon Presents qui débute à 15h ce jeudi 27 février. La publication a été très rapidement supprimée, mais il n'a pas fallu longtemps avant que son contenu fasse le tour d'Internet. Cette vidéo capture une ouverture de booster de la collection Triumphant Light (Lumière Triomphante en français). Le Pokémon légendaire en tête d’affiche de ce nouveau set de cartes n’est nul autre qu’Arceus, le monstre légendaire qui prête son nom au jeu Légendes Pokémon : Arceus. De plus, nous voyons quelques cartes du set : Magnéti, Évoli, Nosferalto (dans des versions alternatives à celles déjà présentes dans le jeu), et la fameuse Arceus EX en relief. On voit d’ailleurs dans la scène suivante un coup porté par cette puissante carte contre un Pokémon adverse. La vidéo se termine par un rappel : deux boosters sont offerts gratuitement chaque jour aux utilisateurs.",
+    subtitle2: "Parvenir à faire revenir les joueurs",
+    text2:
+      "L’année a plutôt mal débuté pour Pokémon Pocket. Les joueurs, qui attendaient beaucoup de la nouvelle fonctionnalité d’échange de cartes, ont été très déçus par le système. Il faut en effet dépenser deux types de ressources pour envoyer votre carte à votre ami : les sabliers d’échange et les jetons d’échange. De plus, plusieurs conditions ont été mises en place pour effectuer les transactions, rendant le tout beaucoup plus compliqué que ce que les joueurs imaginaient. Ce nouveau set de cartes s'ajoute aux 6 types de boosters déjà disponibles sur l’application, à savoir la Puissance Génétique (Mewtwo, Pikachu, Dracaufeu), Île Fabuleuse (Mew), et Spatio-Temporel (avec Dialga et Palkia). Comme avec les autres arrivées de packs, les points Booster qui permettent d’acheter une carte précise ne se réinitialisent pas. C’est-à-dire que la ressource est associée uniquement aux boosters de la collection que vous ouvrez. Le Pokémon Presents d’aujourd’hui tombe le jour des 29 ans de la franchise. C’est en effet le 27 février 1996 que sortait le tout premier jeu : Pokémon Version Rouge et Verte, sur Game Boy.",
+      source: "https://www.jeuxvideo.com/news/1974216/pokemon-pocket-l-une-des-surprises-du-pokemon-presents-vient-d-etre-gachee-on-connait-le-prochain-booster-et-sa-carte-phare.htm"
+  },
+];
+
+export { articles };
